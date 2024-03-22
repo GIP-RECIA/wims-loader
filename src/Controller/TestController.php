163fa7be50d62d5eb8f8ad9648fbd73a4df0fb48
lvillanne-recia <loic.villanne@recia.fr>
2024-03-22 14:02:07 +0100

    feat: ajout user dans test controller

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,18 +2,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
     #[Route(path:"/test", name:"test")]
-    public function number(): Response
+    public function test(Security $security): Response
     {
         $number = random_int(0, 100);
+        $user = $security->getUser();
 
         return new Response(
-            '<html><body>number: '.$number.'</body></html>'
+            '<html><body>user: ' . $user->getUserIdentifier().'<br>'
+            . $number . '</body></html>'
         );
     }
 }
